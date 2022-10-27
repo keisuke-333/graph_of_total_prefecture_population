10df664a130087275c18b6b10759903e0cef5972
keisuke-333 <keikubo.999@gmail.com>
2022-10-28 01:01:49 +0900

    add suspense

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
 import type { NextPage } from 'next'
+import { Suspense } from 'react'
+import { ErrorBoundary } from 'react-error-boundary'
 
 import Graph from '@/components/Graph'
 import Layout from '@/components/Layout'
 import PrefectureList from '@/components/PrefectureList'
+import Spinner from '@/components/Spinner'
 import { usePopulation } from '@/hooks/usePopulation'
 import { usePrefectures } from '@/hooks/usePrefectures'
 
@@ -12,7 +15,13 @@ const HomePage: NextPage = () => {
 
   return (
     <Layout>
-      <PrefectureList data={data} onChange={getPopulation} />
+      <ErrorBoundary
+        fallbackRender={({ error }) => <p className='errorMessage'>Error! {error.message}</p>}
+      >
+        <Suspense fallback={<Spinner />}>
+          <PrefectureList data={data} onChange={getPopulation} />
+        </Suspense>
+      </ErrorBoundary>
       <Graph population={population} />
     </Layout>
   )
