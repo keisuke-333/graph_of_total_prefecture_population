190315c2402f68be3867f7f7f54a0f024572cfae
keisuke-333 <keikubo.999@gmail.com>
2022-10-27 17:28:42 +0900

    setup react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,25 @@
 import '../styles/globals.scss'
 import '../styles/reset.scss'
 
+import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+
+import { queryClient } from '@/libs/queryClient'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>都道府県別総人口推移グラフ</title>
-        <meta name='description' content='都道府県別の総人口推移グラフを表示する SPA' />
+        <meta name='description' content='都道府県別の総人口推移グラフを表示するSPA' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </>
   )
 }
